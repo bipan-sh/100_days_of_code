ce708a864e00c59137f806ea7e4144c36d7cb3d1
Bipan Sharma <sharma.bipan05@gmail.com>
2021-11-06 13:41:29 +1100

    initial commit

---
+++ day1/day1.py
@@ -0,0 +1,8 @@
+# print(len(input('What is your name? ')))
+
+#Band name generator
+print('Welcome to the Band Name Generator')
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+band_name = city + " " + pet
+print(f"Your band name is {band_name}.")
---
+++ day4/heads_tail.py
@@ -0,0 +1,9 @@
+import random
+
+random_integer = random.randint(1, 2)
+
+if random_integer == 1 :
+    print('Heads')
+
+else :
+    print('Tails')
