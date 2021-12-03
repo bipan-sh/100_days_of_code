f43d8e7e48d53b64328eb257867ddcfe5cce77ec
rararas-putin <60082269+rararas-putin@users.noreply.github.com>
2021-12-04 08:53:13 +1100

    added day 10 (#11)

---
+++ day10/calculator.py
@@ -0,0 +1,50 @@
+
+#add
+from art import logo
+import os
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+print(logo)
+
+operations = { '+': add,
+               '-': subtract,
+               '/': divide,
+               '*': multiply,
+}
+
+def calculator():
+    print(logo)
+    num1 = int(input("what's the first number? "))
+    should_continue = True
+
+    while should_continue:
+        num2 = int(input("what's the other number? "))
+
+        for operators in operations:
+            print(operators)
+        operation_symbol = input("Pick an operation from the line above: ")
+        calculation_symbol = operations[operation_symbol]
+        answer = calculation_symbol(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} is {answer}" )
+
+        if input("Enter y to continue or n to exit: ") == 'y':
+            should_continue = True
+            num1 = answer
+        else:
+            should_continue = False
+            os.system('cls')
+            calculator()
+
+calculator()
